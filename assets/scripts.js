816bf3442da82a405d1dc7ae113759af9cc4fce7
Chulps <chuckoward@gmail.com>
2018-02-19 00:01:59 -0800

    another fix

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,7 +5,7 @@ var random_crystal_2 = Math.floor(Math.random() * 12) + 1;
 var random_crystal_3 = Math.floor(Math.random() * 12) + 1;
 var random_crystal_4 = Math.floor(Math.random() * 12) + 1;
 var click_sum = 0; 
-var wins = 0
+var wins = 0;
 var losses = 0;
 document.getElementById("sum_display").innerHTML = random_sum;
 document.getElementById("crystal_1").innerHTML = random_crystal_1;
@@ -185,10 +185,5 @@ document.getElementById("click_sum").innerHTML = click_sum;
       alert ("Nice! That's a win!");
      }
 	});
-  
-  
-  
-  
-
-
+ 
 });
